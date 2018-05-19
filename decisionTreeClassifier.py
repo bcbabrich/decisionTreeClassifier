6ad0ed101fafbcb0105344e5989bf4e9775de0e6
bcbabrich <bcbabrich@gmail.com>
2018-05-19 18:43:18 -0400

    made finding feature with highest value its own method. Also abstracted labels. Next thing to do is set up recursive method of splitting up the dataArray and using returnFeatWithHighestScore

--- decisionTreeClassifier.py
+++ decisionTreeClassifier.py
@@ -1,60 +1,81 @@
-# IN: a list of categories
-# OUT: the index of the category with the highest "score"
-# "score" is defined by Daume in Decision trees
-#def retFeatWHighestScore(features) :
-
-
 # import anytree so we can "train" our decision tree
 from anytree import Node, RenderTree
 
+# RETURN FEATURE WITH HIGHEST "SCORE"
+# IN: features, categories, and labels (AS LISTS)
+# OUT: the index of the category with the highest "score"
+# -> "score" is defined by Daume in Decision trees
+def featureWithHighestScore(features, labels) :
+    highestScore = 0
+    featWHighestScore = 0
+    for f in features :
+        # grab all the different values feature f can have
+        categories = []  
+        for i in range(len(dataArray)) :
+            if dataArray[i][f] not in categories :
+                categories.append(dataArray[i][f])
+
+        corGuess = 0
+
+        # for each category,
+        # count how many times label l appears,
+        # and save the highest count
+        for c in categories :
+            highestNumOfCorGuesses = 0
+            highestCorPerc = 0
+            for l in labels :
+                cTot = 0
+                tempCorGuess = 0
+                for i in range(len(dataArray)) :
+                    if dataArray[i][f] == c :
+                        cTot += 1
+                        if dataArray[i][len(dataArray[0]) - 1].rstrip() == l :
+                            tempCorGuess += 1
+
+                if tempCorGuess > highestNumOfCorGuesses :
+                    highestNumOfCorGuesses = tempCorGuess
+                    
+            # once the highest count has been found for category c,
+            # add it to a total tally 
+            corGuess += highestNumOfCorGuesses
+
+        # the total tally represents our number of correct "guesse" (Daume, p. 12)
+        # this total tally divided by the number of examples gives us the score of feature f
+        # save the highest score (this is what we will return)
+        if corGuess/len(dataArray) > highestScore :
+            highestScore = corGuess/len(dataArray)
+            featWHighestScore = f
+    print('feat with highest score: ' + str(featWHighestScore))
+    return featWHighestScore
+
+################## CONTROL FLOW BEGINS HERE ########################
+    
 #the first thing we need to to is read in our data file and transform it into a 2d-array
 dataFile = open('courseRatings.data','r')
 
-dataArray = []
+dataArray = [] # our initial data array (to be "divided and conqured" into others)
 
 for line in dataFile :
-    dataArray.append(line.split(','))
+    dataArray.append(line.split(',')) # At this point EOL chars are still present in the labels
 
-# note that at this point EOL chars are still present
-# right now, feature f is just the first feature
-
+# create features as a list of ints
 features = list(range(len(dataArray[0]) - 2))
 
-highestScore = 0
-featWHighestScore = 0
-print(features)
+# let's grab all the possible labels a feature f can have
+labels = []
 
-for f in features :
-    categories = [] # all the different values feature f can have 
-    for i in range(len(dataArray)) :
-        if dataArray[i][f] not in categories :
-            categories.append(dataArray[i][f])
+for i in range(len(dataArray)) :
+    if dataArray[i][len(dataArray[0]) - 1].rstrip() not in labels :
+        labels.append(dataArray[i][len(dataArray[0]) - 1].rstrip())
 
-    corGuess = 0
-    
-    for c in categories :
-        cTot = 0
-        tempCorGuess = 0
-        for i in range(len(dataArray)) :
-            if dataArray[i][f] == c :
-                cTot += 1
-                if dataArray[i][len(dataArray[0]) - 1].rstrip() == 'l' :
-                    tempCorGuess += 1
-        if cTot - tempCorGuess > tempCorGuess :
-            tempCorGuess = cTot - tempCorGuess
-        corGuess += tempCorGuess
-
-    if corGuess/len(dataArray) > highestScore :
-        highestScore = corGuess/len(dataArray)
-        featWHighestScore = f
-print('feat with highest score: ' + str(featWHighestScore))
-
+featWHighestScore = featureWithHighestScore(features, labels) # initial first call
+ 
 # Feature with highest score has been found (call if f')
 
 # we now create a new features list with f' removed
 newFeatures = features
 newFeatures.remove(featWHighestScore)
-print(newFeatures)
+#print(newFeatures)
 
 # now we split up dataArray into subsets based on the categories of f'
 
@@ -63,8 +84,8 @@ for i in range(len(dataArray)) :
     if dataArray[i][featWHighestScore] not in categories :
         categories.append(dataArray[i][featWHighestScore])
 
-print(categories)
-print(featWHighestScore)
+#print(categories)
+#print(featWHighestScore)
 
 for c in categories :
     dataArrayOfC = []
